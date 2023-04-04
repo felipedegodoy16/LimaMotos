18d735c0ae1e612b7385f5aad7dd7eec562c7b08
Felipe Godoy <felipedegodoy.dev@gmail.com>
2023-04-03 21:54:27 -0300

    criação de index php e melhora na navbar

--- Páginas/principal/index.php
+++ Páginas/principal/index.php
@@ -16,45 +16,7 @@
     
   </head>
   <body>
-    <header>
-      <div class="container-inline nav-logo">
-          <img src="img/logo.png" alt="Logo">
-      </div>
-      <nav class="navbar navbar-expand-md fixed-top">
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link disabled" href="#">Home <span class="sr-only">(atual)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" href="#">CT Lima Racing</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" href="#">Sobre Nós</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" href="#">A MXF</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" href="#">Especialização em Amortecedores</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" href="#">Manutenção e Acessórios</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" href="#">Linha Urbana</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" href="#">Linha Off Road</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" href="#">Localização</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </header>
-
+    <?php include 'Menu/navbar.php'?>
       <!--Carousel-->
     <div id="myCarousel" class="carousel slide" data-ride="carousel">
       <ol class="carousel-indicators">
